ca62e14dc1afefdf2a17de754935a6dc6470eea2
Adrian Dumbravă <81356778+AdrianDumbrava@users.noreply.github.com>
2024-02-09 18:13:44 +0200

    Create README.md

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,29 +3,14 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends AbstractController
 {
-    #[Route(path: '/test12', name: 'test12')]
-    public function test(Request $request): Response
+    #[Route(path: '/test', name: 'test')]
+    public function test(): Response
     {
-        $test = 1;
-
-        $test = $test + 2;
-
-        return new Response(
-            '<html><body>Yeeey, merge!!</body></html>'
-        );
-    }
-
-    #[Route(path: '/ruta', name: 'test')]
-    public function test2(Request $request): Response
-    {
-        return new Response(
-            '<html><body>TEST, ROUTE NR 2!!</body></html>'
-        );
+        return $this->render('test.html.twig');
     }
 }
